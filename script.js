ceae144ed1a481db3ad819518006ca53d0a100fe
Rachel La <rachel.la1807@gmail.com>
2022-07-23 14:38:36 -0400

    Added real time clock

---
+++ script.js
@@ -0,0 +1,17 @@
+function realtimeClock() {
+    var rtClock = new Date();
+    var hour = rtClock.getHours();
+    var minute = rtClock.getMinutes();
+    var second = rtClock.getSeconds();
+
+    var ampm = (hour < 12) ? "AM" : "PM";
+
+    hour = (hour > 12) ? hour - 12 : hour;
+    hour = ("0" + hour).slice(-2);
+    minute = ("0" + minute).slice(-2);
+    second = ("0" + second).slice(-2);
+    document.getElementById('clock').innerHTML =
+        hour + " : " + minute + " : " 
+        + second + " " + ampm;
+    var t = setTimeout(realtimeClock, 500);
+}
