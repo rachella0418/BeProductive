e94ac88d8e6af81ce1956c063d7f35e1028ceec3
Rachel La <rachel.la1807@gmail.com>
2022-08-28 16:29:04 -0400

    Finalized

--- script.js
+++ script.js
@@ -43,11 +43,55 @@ function addTask(event) {
     newTask.innerHTML = "new task";
     newTask.classList.add('todo-item');
     todoDiv.appendChild(newTask);
-
     taskList.appendChild(todoDiv);
 }
 
-const song = document.getElementById("music");
-function e(event) {
-    song.play();
+var slider = document.querySelector(".slider input");
+var progressBar = document.querySelector(".slider progress");
+var rain = document.getElementById("rain");
+var wavesSlider = document.querySelector(".waves-slider input");
+var wavesProgressBar = document.querySelector(".waves-slider progress");
+var waves = document.getElementById("waves");
+var citySlider = document.querySelector(".city-slider input");
+var cityProgressBar = document.querySelector(".city-slider progress");
+var city = document.getElementById("city");
+var lofiSlider = document.querySelector(".lofi-slider input");
+var lofiProgressBar = document.querySelector(".lofi-slider progress");
+var lofi = document.getElementById("lofi");
+
+rain.play();
+waves.play();
+city.play();
+lofi.play();
+waves.loop = true;
+rain.loop = true;
+city.loop = true;
+lofi.loop = true;
+
+slider.oninput = function() {
+    progressBar.value = slider.value;
 }
+wavesSlider.oninput = function() {
+    wavesProgressBar.value = wavesSlider.value;
+}
+citySlider.oninput = function() {
+    cityProgressBar.value = citySlider.value;
+}
+lofiSlider.oninput = function() {
+    lofiProgressBar.value = lofiSlider.value;
+}
+
+slider.addEventListener("mousemove", rain.volume = slider.value / 100);
+wavesSlider.addEventListener("mousemove", waves.volum = wavesSlider.value / 100);
+citySlider.addEventListener("mousemove", city.volume = citySlider.value / 100);
+lofiSlider.addEventListener("mousemove", lofi.volume = lofiSlider.value / 100);
+
+/*function setVolume() {
+    rain.volume = slider.value / 100;
+    waves.volume = wavesSlider.value / 100;
+    city.volume = citySlider.value / 100;
+    lofi.volume = lofiSlider.value / 100;
+}*/
+
+
+
